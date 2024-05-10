7beba114006574bd5d5be09b9c0c6bfa9ff23ed0
almogbaku <almog.baku@gmail.com>
2024-05-10 14:16:30 +0300

    fix: rating is wrong

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -9,8 +9,8 @@ export const Rating = ({value, onChange}: { value?: number, onChange: (v: number
         <input type="radio" className="rating-hidden" checked={!value} readOnly/>
         {range(5).map((i) => (
             <Fragment key={i}><input type="radio"
-                                     className={`mask mask-star-2 mask-half-1`} onChange={() => onChange(i+0.5)}
-                                     checked={value ? value == i+0.5 : false}/>
+                                     className={`mask mask-star-2 mask-half-1`} onChange={() => onChange(i + 0.5)}
+                                     checked={value ? value == i + 0.5 : false}/>
                 <input type="radio"
                        className={`mask mask-star-2 mask-half-2 mr-0.5`} onChange={() => onChange(i)}
                        checked={value ? value == i : false}/></Fragment>
